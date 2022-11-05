68036a098325862902ec14519fe635c61f03d065
xhayper <hayper1919@gmail.com>
2022-11-05 13:23:00 +0700

    chore: dependencies update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: true,
-  target: "server",
   modules: [
     "@nuxt/image-edge",
     "@vueuse/nuxt",
-    "nuxt-windicss",
-    "@nuxtjs/robots",
-    "@funken-studio/sitemap-nuxt-3",
+    "nuxt-windicss"
   ],
   css: ["@/assets/styles/global.scss"],
-  robots: {
-    Sitemap: (req: any) => `https://${req.headers.host}/sitemap.xml`,
-  },
-  sitemap: {
-    hostname: "http://portfolio-xhayper.vercel.app",
-  },
-  sourcemap: {
-    client: true,
-    server: true,
-  },
-  vite: {
-    build: {
-      sourcemap: true,
-    },
-  },
   typescript: {
     shim: false,
     strict: true,
