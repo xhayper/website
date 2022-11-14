89b59df8d2abd0b9f74018f9429543341fc49d46
xhayper <hayper1919@gmail.com>
2022-11-14 22:40:27 +0700

    remove: unneeded package feat: fix font

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,19 +1,10 @@
 import transformerDirectives from "@unocss/transformer-directives";
-import presetWebFonts from "@unocss/preset-web-fonts";
-import presetIcons from "@unocss/preset-icons";
 import presetWind from "@unocss/preset-wind";
 import { defineConfig } from "unocss";
 
 export default defineConfig({
   transformers: [transformerDirectives()],
   presets: [
-    presetWind(),
-    presetIcons(),
-    presetWebFonts({
-      provider: "none",
-      fonts: {
-        sans: "Inter var",
-      },
-    }),
+    presetWind()
   ],
 });
