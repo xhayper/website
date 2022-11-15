ac05fc9ef2f1c33690ac36aa58048bdff5f2425f
xhayper <hayper1919@gmail.com>
2022-11-15 10:12:44 +0700

    feat: all my homie hate rem

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,10 +1,9 @@
 import transformerDirectives from "@unocss/transformer-directives";
+import presetRemToPx from "@unocss/preset-rem-to-px";
 import presetWind from "@unocss/preset-wind";
 import { defineConfig } from "unocss";
 
 export default defineConfig({
   transformers: [transformerDirectives()],
-  presets: [
-    presetWind()
-  ],
+  presets: [presetWind(), presetRemToPx()],
 });
