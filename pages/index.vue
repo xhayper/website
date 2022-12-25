fbb809b75b7d505bc80cf9ab236cef4149d7db3b
xhayper <hayper1919@gmail.com>
2022-12-25 10:35:50 +0700

    feat: add information in main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,54 @@
 <template>
-  <div />
+  <div>
+    <ul>
+      <li>
+        Former developer at <a href="https://www.roblox.com/games/6682504402" target="_blank"
+        rel="noreferrer noopener">Altered</a>
+      </li>
+      <li>
+        Former developer at <a href="https://www.roblox.com/games/6464459617" target="_blank"
+        rel="noreferrer noopener">RESURGENCED RP</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        Former QA-Tester <a href="https://www.roblox.com/games/6306910204" target="_blank"
+          rel="noreferrer noopener">RESURGENCED</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        Former Delta (Moderator) at <a href="https://discord.gg/lyricwulf" target="_blank"
+          rel="noreferrer noopener">Lyricity</a>
+      </li>
+      <li>
+        Former moderator at <a href="https://www.roblox.com/games/6456351776" target="_blank"
+          rel="noreferrer noopener">Kaiju Paradise</a>
+      </li>
+      <li>
+        Former moderator at <a href="https://www.reddit.com/r/CultOfTheLamb" target="_blank"
+          rel="noreferrer noopener">r/CultOfTheLamb</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        Developer at <a href="https://www.roblox.com/games/7131355525" target="_blank" rel="noreferrer noopener">
+          Thunder Scientific Corporation</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        Moderator at <a href="https://www.roblox.com/games/10875701453" target="_blank" rel="noreferrer noopener">Edward
+          the Man Eating Train</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <a href="https://github.com/xhayper" target="_blank" rel="noreferrer noopener">GitHub</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/hayper1919" target="_blank" rel="noreferrer noopener">Twitter</a>
+      </li>
+    </ul>
+  </div>
 </template>
