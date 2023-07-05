65277e91c465ff97aace8516f9548b3a1f93f6d7
hayper <hayper1919@gmail.com>
2023-07-05 18:14:14 +0700

    feat: re-order stuff

--- pages/index.vue
+++ pages/index.vue
@@ -2,32 +2,6 @@
   <div>
     <ul>
       <li>
-        Former developer at <a href="https://www.roblox.com/games/6682504402" target="_blank"
-          rel="noreferrer noopener">Altered</a>
-      </li>
-      <li>
-        Former developer at <a href="https://www.roblox.com/games/6464459617" target="_blank"
-          rel="noreferrer noopener">RESURGENCED RP</a>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        Former QA-Tester <a href="https://www.roblox.com/games/6306910204" target="_blank"
-          rel="noreferrer noopener">RESURGENCED</a>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        Former Delta (Moderator) at <a href="https://discord.gg/lyricwulf" target="_blank"
-          rel="noreferrer noopener">Lyricity</a>
-      </li>
-      <li>
-        Former moderator at <a href="https://www.roblox.com/games/6456351776" target="_blank"
-          rel="noreferrer noopener">Kaiju Paradise</a>
-      </li>
-    </ul>
-    <ul>
-      <li>
         Discord bot developer at <a href="https://discord.gg/changed" target="_blank"
           rel="noreferrer noopener">Changed</a>
       </li>
@@ -59,6 +33,32 @@
       <li>
         Moderator at <a href="https://www.reddit.com/r/CultOfTheLamb" target="_blank"
           rel="noreferrer noopener">r/CultOfTheLamb</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        Former developer at <a href="https://www.roblox.com/games/6682504402" target="_blank"
+          rel="noreferrer noopener">Altered</a>
+      </li>
+      <li>
+        Former developer at <a href="https://www.roblox.com/games/6464459617" target="_blank"
+          rel="noreferrer noopener">RESURGENCED RP</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        Former QA-Tester <a href="https://www.roblox.com/games/6306910204" target="_blank"
+          rel="noreferrer noopener">RESURGENCED</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        Former Delta (Moderator) at <a href="https://discord.gg/lyricwulf" target="_blank"
+          rel="noreferrer noopener">Lyricity</a>
+      </li>
+      <li>
+        Former moderator at <a href="https://www.roblox.com/games/6456351776" target="_blank"
+          rel="noreferrer noopener">Kaiju Paradise</a>
       </li>
     </ul>
     <ul>
