5210847481226aaeb271d266609bf1289781e993
hayper <hayper1919@gmail.com>
2023-10-21 09:25:44 +0700

    feat: add backup link for Unhappy Raccoon

--- pages/index.vue
+++ pages/index.vue
@@ -2,82 +2,182 @@
   <div>
     <ul>
       <li>
-        Discord bot developer at <a href="https://discord.gg/changed" target="_blank"
-          rel="noreferrer noopener">Changed</a>
+        Discord bot developer at
+        <a
+          href="https://discord.gg/changed"
+          target="_blank"
+          rel="noreferrer noopener"
+          >Changed</a
+        >
       </li>
       <li>
-        Developer at <a href="https://www.roblox.com/games/7131355525" target="_blank" rel="noreferrer noopener">
-          Thunder Scientific Corporation</a>
+        Developer at
+        <a
+          href="https://www.roblox.com/games/7131355525"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Thunder Scientific Corporation</a
+        >
       </li>
       <li>
-        Developer at <a href="https://www.roblox.com/games/6675127933" target="_blank" rel="noreferrer noopener">
-          The Petting Room</a>
+        Developer at
+        <a
+          href="https://www.roblox.com/games/6675127933"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          The Petting Room</a
+        >
       </li>
       <li>
-        Developer at <a href="https://www.roblox.com/groups/15809968" target="_blank" rel="noreferrer noopener">
-          Daybreak.Games</a>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        Discord Moderator at at <a href="https://raccoon.xd.com/" target="_blank" rel="noreferrer noopener">Unhappy
-          Raccoon</a>
-      </li>
-      <li>
-        Moderator at <a href="https://discord.gg/changed" target="_blank" rel="noreferrer noopener">Changed</a>
-      </li>
-      <li>
-        Moderator at <a href="https://www.roblox.com/games/10875701453" target="_blank" rel="noreferrer noopener">Edward
-          the Man Eating Train</a>
-      </li>
-      <li>
-        Moderator at <a href="https://www.reddit.com/r/CultOfTheLamb" target="_blank"
-          rel="noreferrer noopener">r/CultOfTheLamb</a>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        Former developer at <a href="https://www.roblox.com/games/6682504402" target="_blank"
-          rel="noreferrer noopener">Altered</a>
-      </li>
-      <li>
-        Former developer at <a href="https://www.roblox.com/games/6464459617" target="_blank"
-          rel="noreferrer noopener">RESURGENCED RP</a>
+        Developer at
+        <a
+          href="https://www.roblox.com/groups/15809968"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Daybreak.Games</a
+        >
       </li>
     </ul>
     <ul>
       <li>
-        Former QA-Tester <a href="https://www.roblox.com/games/6306910204" target="_blank"
-          rel="noreferrer noopener">RESURGENCED</a>
+        Discord Moderator at at
+        <a
+          href="https://raccoon.xd.com/"
+          target="_blank"
+          rel="noreferrer noopener"
+          >Unhappy Raccoon</a
+        >
+        [<a
+          href="https://discord.gg/fNZ8CHVJvy"
+          target="_blank"
+          rel="noreferrer noopener"
+          >Backup link</a
+        >]
+      </li>
+      <li>
+        Moderator at
+        <a
+          href="https://discord.gg/changed"
+          target="_blank"
+          rel="noreferrer noopener"
+          >Changed</a
+        >
+      </li>
+      <li>
+        Moderator at
+        <a
+          href="https://www.roblox.com/games/10875701453"
+          target="_blank"
+          rel="noreferrer noopener"
+          >Edward the Man Eating Train</a
+        >
+      </li>
+      <li>
+        Moderator at
+        <a
+          href="https://www.reddit.com/r/CultOfTheLamb"
+          target="_blank"
+          rel="noreferrer noopener"
+          >r/CultOfTheLamb</a
+        >
       </li>
     </ul>
     <ul>
       <li>
-        Former Delta (Moderator) at <a href="https://discord.gg/lyricwulf" target="_blank"
-          rel="noreferrer noopener">Lyricity</a>
+        Former developer at
+        <a
+          href="https://www.roblox.com/games/6682504402"
+          target="_blank"
+          rel="noreferrer noopener"
+          >Altered</a
+        >
       </li>
       <li>
-        Former moderator at <a href="https://www.roblox.com/games/6456351776" target="_blank"
-          rel="noreferrer noopener">Kaiju Paradise</a>
+        Former developer at
+        <a
+          href="https://www.roblox.com/games/6464459617"
+          target="_blank"
+          rel="noreferrer noopener"
+          >RESURGENCED RP</a
+        >
       </li>
     </ul>
     <ul>
       <li>
-        <a href="https://github.com/xhayper" target="_blank" rel="noreferrer noopener">GitHub</a>
+        Former QA-Tester
+        <a
+          href="https://www.roblox.com/games/6306910204"
+          target="_blank"
+          rel="noreferrer noopener"
+          >RESURGENCED</a
+        >
+      </li>
+    </ul>
+    <ul>
+      <li>
+        Former Delta (Moderator) at
+        <a
+          href="https://discord.gg/lyricwulf"
+          target="_blank"
+          rel="noreferrer noopener"
+          >Lyricity</a
+        >
       </li>
       <li>
-        <a href="https://twitter.com/hayper1919" target="_blank" rel="noreferrer noopener">Twitter</a>
+        Former moderator at
+        <a
+          href="https://www.roblox.com/games/6456351776"
+          target="_blank"
+          rel="noreferrer noopener"
+          >Kaiju Paradise</a
+        >
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/xhayper"
+          target="_blank"
+          rel="noreferrer noopener"
+          >GitHub</a
+        >
       </li>
       <li>
-        <a href="https://furry.engineer/@hayper" target="_blank" rel="noreferrer noopener">Mastodon</a>
+        <a
+          href="https://twitter.com/hayper1919"
+          target="_blank"
+          rel="noreferrer noopener"
+          >Twitter</a
+        >
       </li>
       <li>
-        <a href="https://bsky.app/profile/hayper.bsky.social" target="_blank" rel="noreferrer noopener">Bluesky</a>
+        <a
+          href="https://furry.engineer/@hayper"
+          target="_blank"
+          rel="noreferrer noopener"
+          >Mastodon</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://bsky.app/profile/hayper.bsky.social"
+          target="_blank"
+          rel="noreferrer noopener"
+          >Bluesky</a
+        >
       </li>
       <li>Discord: xhayper</li>
       <li>Revolt: hayper#5357</li>
       <li>
-        <a href="https://discord.gg/xTAR8nUs2g" target="_blank" rel="noreferrer noopener">Discord Server</a>
+        <a
+          href="https://discord.gg/xTAR8nUs2g"
+          target="_blank"
+          rel="noreferrer noopener"
+          >Discord Server</a
+        >
       </li>
     </ul>
   </div>
