454ff03322bf7cb20e823fbb3114589f8118b252
hayper <hayper1919@gmail.com>
2024-02-08 17:51:02 +0700

    feat: update page

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,6 @@
   <div>
     <ul>
       <li>
-        Discord bot developer at
-        <a
-          href="https://discord.gg/changed"
-          target="_blank"
-          rel="noreferrer noopener"
-          >Changed</a
-        >
-      </li>
-      <li>
         Developer at
         <a
           href="https://github.com/reflektone-games"
@@ -18,16 +9,6 @@
           rel="noreferrer noopener"
         >
           Reflektone Games</a
-        >
-      </li>
-      <li>
-        Developer at
-        <a
-          href="https://www.roblox.com/games/7131355525"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Thunder Scientific Corporation</a
         >
       </li>
       <li>
@@ -70,15 +51,6 @@
       <li>
         Moderator at
         <a
-          href="https://discord.gg/changed"
-          target="_blank"
-          rel="noreferrer noopener"
-          >Changed</a
-        >
-      </li>
-      <li>
-        Moderator at
-        <a
           href="https://www.roblox.com/games/10875701453"
           target="_blank"
           rel="noreferrer noopener"
@@ -92,57 +64,6 @@
           target="_blank"
           rel="noreferrer noopener"
           >r/CultOfTheLamb</a
-        >
-      </li>
-    </ul>
-    <ul>
-      <li>
-        Former developer at
-        <a
-          href="https://www.roblox.com/games/6682504402"
-          target="_blank"
-          rel="noreferrer noopener"
-          >Altered</a
-        >
-      </li>
-      <li>
-        Former developer at
-        <a
-          href="https://www.roblox.com/games/6464459617"
-          target="_blank"
-          rel="noreferrer noopener"
-          >RESURGENCED RP</a
-        >
-      </li>
-    </ul>
-    <ul>
-      <li>
-        Former QA-Tester
-        <a
-          href="https://www.roblox.com/games/6306910204"
-          target="_blank"
-          rel="noreferrer noopener"
-          >RESURGENCED</a
-        >
-      </li>
-    </ul>
-    <ul>
-      <li>
-        Former Delta (Moderator) at
-        <a
-          href="https://discord.gg/lyricwulf"
-          target="_blank"
-          rel="noreferrer noopener"
-          >Lyricity</a
-        >
-      </li>
-      <li>
-        Former moderator at
-        <a
-          href="https://www.roblox.com/games/6456351776"
-          target="_blank"
-          rel="noreferrer noopener"
-          >Kaiju Paradise</a
         >
       </li>
     </ul>
