a9227ca13e7a36522d38cbe101a553b5cd9d2ba9
xhayper <hayper1919@gmail.com>
2024-02-18 00:11:07 +0700

    feat: add vercel speed insights

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
+        <SpeedInsights />
         <Navbar />
         <slot />
     </div>
 </template>
 
 <script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+
 updateTitle();
 </script>
