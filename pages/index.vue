b9c8e04da1f52437739ce9bb025e51d58b8281e5
xhayper <hayper1919@gmail.com>
2024-02-24 14:55:00 +0700

    feat: finally update hte main page abit

--- pages/index.vue
+++ pages/index.vue
@@ -1,76 +1,31 @@
 <template>
     <div class="m-5">
-        <ul>
-            <li>
-                <NuxtLink to="https://github.com/xhayper" target="_blank" rel="me noreferrer noopener"
-                    class="text-gray-900 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
-                    GitHub</NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="https://twitter.com/hayper1919" target="_blank" rel="me noreferrer noopener"
-                    class="text-gray-900 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
-                    Twitter
-                </NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="https://furry.engineer/@hayper" target="_blank" rel="me noreferrer noopener"
-                    class="text-gray-900 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
-                    Mastodon
-                </NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="https://bsky.app/profile/hayper.xyz" target="_blank" rel="me noreferrer noopener"
-                    class="text-gray-900 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
-                    Bluesky
-                </NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="https://www.roblox.com/users/1921023125/profile" target="_blank" rel="me noreferrer noopener"
-                    class="text-gray-900 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
-                    Roblox
-                </NuxtLink>
-            </li>
-            <li>
-                <NuxtLink to="https://discord.gg/xTAR8nUs2g" target="_blank" rel="noreferrer noopener"
-                    class="text-gray-900 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
-                    Discord Server
-                </NuxtLink>
-            </li>
-        </ul>
+        <div>
+            <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Socials:</h2>
+            <ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+                <li v-for="social in socialList" class="flex items-center">
+                    <NuxtLink v-if="social.url" :to="social.url" target="_blank" rel="me noreferrer noopener"
+                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">{{ social.name }}
+                    </NuxtLink>
+                    <span v-else class="font-medium">{{ social.name }}: {{ social.text }}</span>
+                </li>
+            </ul>
+        </div>
         <br>
-        <ul>
-            <li>Discord: xhayper</li>
-            <li>Revolt: hayper#5357</li>
-        </ul>
-        <br>
-        <ul>
-            <li>
-                <NuxtLink to="https://github.com/xhayper/website/" target="_blank" rel="noreferrer noopener"
-                    class="text-gray-900 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
-                    Source code
-                </NuxtLink>
-            </li>
-        </ul>
-        <br>
-        <br>
-        also this website use vercel analytics so like...<br>
-        more info here: <NuxtLink to="https://vercel.com/docs/analytics/quickstart" target="_blank"
-            rel="noreferrer noopener"
-            class="text-gray-900 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
-            here
-        </NuxtLink>
-        <br>
-        <br>
-        also vercel speed insights so like...<br>
-        more info here: <NuxtLink to="https://vercel.com/docs/speed-insights/quickstart" target="_blank"
-            rel="noreferrer noopener"
-            class="text-gray-900 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white">
-            here
-        </NuxtLink>
+        <div>
+            <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Misc:</h2>
+            <ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+                <li v-for="misc in miscList" class="flex items-center">
+                    <NuxtLink :to="misc.url" target="_blank" rel="me noreferrer noopener"
+                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">{{ misc.name }}
+                    </NuxtLink>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="tsx">
 definePageMeta({
     title: "hayper",
     ogTitle: "hayper",
@@ -78,5 +33,19 @@ definePageMeta({
     overrideTitle: true
 });
 
-// TODO: Make it so the link are using an array of objects
+const socialList = [
+    { "name": "GitHub", "url": "https://github.com/xhayper", },
+    { "name": "Twitter", "url": "https://twitter.com/hayper1919", },
+    { "name": "Mastodon", "url": "https://furry.engineer/@hayper" },
+    { "name": "Bluesky", "url": "https://bsky.app/profile/hayper.xyz" },
+    { "name": "Roblox", "url": "https://www.roblox.com/users/1921023125/profile" },
+    { "name": "Discord Server", "url": "https://discord.gg/xTAR8nUs2g", },
+
+    { "name": "Discord", "text": "xhayper" },
+    { "name": "Revolt", "text": "hayper#5357", },
+]
+
+const miscList = [
+    { "name": "Source code", "url": "https://github.com/xhayper/website" }
+];
 </script>
